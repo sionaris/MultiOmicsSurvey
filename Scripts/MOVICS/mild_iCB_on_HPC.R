61f10d620fd1dfac6f4d7e849a2ff727cfcfcf94
Aris Sionakidis <as3582@cam.ac.uk>
2024-07-30 10:54:06 +0100

    iCB HPC cluster scripts

---
+++ Scripts/MOVICS/mild_iCB_on_HPC.R
@@ -0,0 +1,22 @@
+.libPaths(c("/home/as3582/rds/hpc-work/Rlibs/win-library/4.4",
+            "/home/as3582/rds/hpc-work/Rlibs/R-4.4.0/library"))
+
+library(MOVICS)
+
+load("/home/as3582/rds/hpc-work/MultiOmicsSurvey/movics_env.RData")
+
+iClusterBayes.res = getMOIC(data        = input,
+                            N.clust     = 3,
+                            methodslist = "iClusterBayes",
+                            type        = c("gaussian",
+                                            "gaussian",
+                                            "gaussian",
+                                            "gaussian",
+                                            "binomial"),
+                            n.burnin    = 1800,
+                            n.draw      = 1200,
+                            prior.gamma = c(0.5, 0.5, 0.5, 0.5, 0.5),
+                            sdev        = 0.05,
+                            thin        = 3)
+
+saveRDS(iClusterBayes.res, "/home/as3582/rds/hpc-work/MultiOmicsSurvey/iCB_mild.rds")
