96832eb57f6322d1b2d7edfbd5326e4ba4021829
Aris Sionakidis <as3582@cam.ac.uk>
2024-09-08 20:01:51 +0100

    Modify CIMLR() to include binary distances

--- Scripts/automated_scripts/custom_functions.R
+++ Scripts/automated_scripts/custom_functions.R
@@ -800,3 +800,299 @@ calculate_S <- function(W) {
   
   return(S)
 }
+
+# Allow non-euclidean distances in CIMLR #####
+dist2.cimlr_mod2 = function (x, c = NA, name = NULL, method = "sqeuclidean") 
+{
+  if (is.na(c)) {
+    c = x
+  }
+  n1 = nrow(x)
+  d1 = ncol(x)
+  n2 = nrow(c)
+  d2 = ncol(c)
+  
+  if (d1 != d2) {
+    stop("Data dimension does not match dimension of centres.")
+  }
+  
+  # Handle different distance metrics
+  if (method == "sqeuclidean") {
+    # Euclidean distance formula
+    dist = t(rep(1, n2) %*% t(apply(t(x^2), MARGIN = 2, FUN = sum))) + 
+      (rep(1, n1) %*% t(apply(t(c^2), MARGIN = 2, FUN = sum))) - 
+      2 * (x %*% t(c))
+  } else if (method == "binary") {
+    dist = as.matrix(dist(as.matrix(x), as.matrix(c), method = "binary"))
+  } else {
+    stop("Unsupported distance method.")
+  }
+  dist[which(dist < 0, arr.ind = TRUE)] = 0
+  # print(paste0(name, ": dim(dist)=", nrow(dist), "x", ncol(dist)))
+  return(dist)
+}
+
+# Multikernel
+multiple.kernel.cimlr_mod = function (x, cores.ratio = 1, name = NULL, method = "sqeuclidean") 
+{
+  kernel.type = list()
+  kernel.type[1] = list("poly")
+  kernel.params = list()
+  kernel.params[1] = list(0)
+  N = dim(x)[1]
+  KK = 0
+  sigma = seq(2, 1, -0.25)
+  Diff = dist2.cimlr_mod2(x, method = method, name = name)
+  Diff_sort = t(apply(Diff, MARGIN = 2, FUN = sort))
+  m = dim(Diff)[1]
+  n = dim(Diff)[2]
+  allk = seq(10, 30, 2)
+  cores = as.integer(cores.ratio * (detectCores() - 1))
+  if (cores < 1 || is.na(cores) || is.null(cores)) {
+    cores = 1
+  }
+  cl = makeCluster(cores)
+  clusterEvalQ(cl, {
+    library(Matrix)
+  })
+  D_Kernels = list()
+  D_Kernels = unlist(parLapply(cl, 1:length(allk), fun = function(l, 
+                                                                  x_fun = x, Diff_sort_fun = Diff_sort, allk_fun = allk, 
+                                                                  Diff_fun = Diff, sigma_fun = sigma, KK_fun = KK) {
+    if (allk_fun[l] < (nrow(x_fun) - 1)) {
+      TT = apply(Diff_sort_fun[, 2:(allk_fun[l] + 1)], 
+                 MARGIN = 1, FUN = mean) + .Machine$double.eps
+      TT = matrix(data = TT, nrow = length(TT), ncol = 1)
+      Sig = apply(array(0, c(nrow(TT), ncol(TT))), MARGIN = 1, 
+                  FUN = function(x) {
+                    x = TT[, 1]
+                  })
+      Sig = Sig + t(Sig)
+      Sig = Sig/2
+      Sig_valid = array(0, c(nrow(Sig), ncol(Sig)))
+      Sig_valid[which(Sig > .Machine$double.eps, arr.ind = TRUE)] = 1
+      Sig = Sig * Sig_valid + .Machine$double.eps
+      for (j in 1:length(sigma_fun)) {
+        W = dnorm(Diff_fun, 0, sigma_fun[j] * Sig)
+        D_Kernels[[KK_fun + l + j]] = Matrix((W + t(W))/2, 
+                                             sparse = TRUE, doDiag = FALSE)
+      }
+      return(D_Kernels)
+    }
+  }))
+  stopCluster(cl)
+  for (i in 1:length(D_Kernels)) {
+    K = D_Kernels[[i]]
+    k = 1/sqrt(diag(K) + 1)
+    G = K * (k %*% t(k))
+    G1 = apply(array(0, c(length(diag(G)), length(diag(G)))), 
+               MARGIN = 2, FUN = function(x) {
+                 x = diag(G)
+               })
+    G2 = t(G1)
+    D_Kernels_tmp = (G1 + G2 - 2 * G)/2
+    D_Kernels_tmp = D_Kernels_tmp - diag(diag(D_Kernels_tmp))
+    D_Kernels[[i]] = Matrix(D_Kernels_tmp, sparse = TRUE, 
+                            doDiag = FALSE)
+  }
+  return(D_Kernels)
+}
+
+
+# CIMLR
+CIMLR_mod = function (X, c, no.dim = NA, k = 10, cores.ratio = 1, binary_flags = NULL,
+                      binary_distance = "binary", nonbinary_distance = "sqeuclidean") 
+{
+  library(CIMLR)
+  if (is.na(no.dim)) {
+    no.dim = c
+  }
+  ptm = proc.time()
+  NITER = 30
+  num = ncol(X[[1]])
+  r = -1
+  beta = 0.8
+  cat("Computing the multiple Kernels.\n")
+  for (data_types in 1:length(X)) {
+    curr_X = X[[data_types]]
+    if (!is.null(binary_flags)) {
+      if (binary_flags[data_types] == "Yes") {
+        method = binary_distance
+      } else {
+        method = nonbinary_distance
+      }
+    } else {
+      method = "sqeuclidean"
+    }
+    if (data_types == 1) {
+      D_Kernels = multiple.kernel.cimlr_mod(t(curr_X), method = method, name = names(X)[data_types])
+    }
+    else {
+      D_Kernels = c(D_Kernels, multiple.kernel.cimlr_mod(t(curr_X), method = method, name = names(X)[data_types]))
+    }
+  }
+  alphaK = 1/rep(length(D_Kernels), length(D_Kernels))
+  distX = array(0, c(dim(D_Kernels[[1]])[1], dim(D_Kernels[[1]])[2]))
+  # for (i in 1:length(D_Kernels)) {
+  #   distX = distX + D_Kernels[[i]]
+  # }
+  for (i in 1:length(D_Kernels)) {
+    # print(paste0("Dimensions of distX: ", dim(distX)[1], "x", dim(distX)[2]))
+    # print(paste0("Dimensions of D_Kernels[[", i, "]]: ", dim(D_Kernels[[i]])[1], "x", dim(D_Kernels[[i]])[2]))
+    
+    # Perform the addition
+    distX = distX + D_Kernels[[i]]
+  }
+  
+  distX = distX/length(D_Kernels)
+  res = apply(distX, MARGIN = 1, FUN = function(x) return(sort(x, 
+                                                               index.return = TRUE)))
+  distX1 = array(0, c(nrow(distX), ncol(distX)))
+  idx = array(0, c(nrow(distX), ncol(distX)))
+  for (i in 1:nrow(distX)) {
+    distX1[i, ] = res[[i]]$x
+    idx[i, ] = res[[i]]$ix
+  }
+  A = array(0, c(num, num))
+  di = distX1[, 2:(k + 2)]
+  rr = 0.5 * (k * di[, k + 1] - apply(di[, 1:k], MARGIN = 1, 
+                                      FUN = sum))
+  id = idx[, 2:(k + 2)]
+  numerator = (apply(array(0, c(length(di[, k + 1]), dim(di)[2])), 
+                     MARGIN = 2, FUN = function(x) {
+                       x = di[, k + 1]
+                     }) - di)
+  temp = (k * di[, k + 1] - apply(di[, 1:k], MARGIN = 1, FUN = sum) + 
+            .Machine$double.eps)
+  denominator = apply(array(0, c(length(temp), dim(di)[2])), 
+                      MARGIN = 2, FUN = function(x) {
+                        x = temp
+                      })
+  temp = numerator/denominator
+  a = apply(array(0, c(length(t(1:num)), dim(di)[2])), MARGIN = 2, 
+            FUN = function(x) {
+              x = 1:num
+            })
+  A[cbind(as.vector(a), as.vector(id))] = as.vector(temp)
+  if (r <= 0) {
+    r = mean(rr)
+  }
+  lambda = max(mean(rr), 0)
+  A[is.nan(A)] = 0
+  S0 = max(max(distX)) - distX
+  cat("Performing network diffusion.\n")
+  S0 = CIMLR:::network.diffusion(S0, k)
+  S0 = CIMLR:::dn.cimlr(S0, "ave")
+  S = (S0 + t(S0))/2
+  D0 = diag(apply(S, MARGIN = 2, FUN = sum))
+  L0 = D0 - S
+  eig1_res = CIMLR:::eig1(L0, c, 0)
+  F_eig1 = eig1_res$eigvec
+  temp_eig1 = eig1_res$eigval
+  evs_eig1 = eig1_res$eigval_full
+  F_eig1 = CIMLR:::dn.cimlr(F_eig1, "ave")
+  converge = vector()
+  for (iter in 1:NITER) {
+    cat("Iteration: ", iter, "\n")
+    distf = CIMLR:::L2_distance_1(t(F_eig1), t(F_eig1))
+    A = array(0, c(num, num))
+    b = idx[, 2:dim(idx)[2]]
+    a = apply(array(0, c(num, ncol(b))), MARGIN = 2, FUN = function(x) {
+      x = 1:num
+    })
+    inda = cbind(as.vector(a), as.vector(b))
+    ad = (distX[inda] + lambda * distf[inda])/2/r
+    dim(ad) = c(num, ncol(b))
+    c_input = -t(ad)
+    c_output = t(ad)
+    ad = t(.Call("projsplx", c_input, c_output))
+    A[inda] = as.vector(ad)
+    A[is.nan(A)] = 0
+    S = (1 - beta) * A + beta * S
+    S = CIMLR:::network.diffusion(S, k)
+    S = (S + t(S))/2
+    D = diag(apply(S, MARGIN = 2, FUN = sum))
+    L = D - S
+    F_old = F_eig1
+    eig1_res = CIMLR:::eig1(L, c, 0)
+    F_eig1 = eig1_res$eigvec
+    temp_eig1 = eig1_res$eigval
+    ev_eig1 = eig1_res$eigval_full
+    F_eig1 = CIMLR:::dn.cimlr(F_eig1, "ave")
+    F_eig1 = (1 - beta) * F_old + beta * F_eig1
+    evs_eig1 = cbind(evs_eig1, ev_eig1)
+    DD = vector()
+    for (i in 1:length(D_Kernels)) {
+      temp = (.Machine$double.eps + D_Kernels[[i]]) * 
+        (S + .Machine$double.eps)
+      DD[i] = mean(apply(temp, MARGIN = 2, FUN = sum))
+    }
+    alphaK0 = CIMLR:::umkl.cimlr(DD)
+    alphaK0 = alphaK0/sum(alphaK0)
+    alphaK = (1 - beta) * alphaK + beta * alphaK0
+    alphaK = alphaK/sum(alphaK)
+    fn1 = sum(ev_eig1[1:c])
+    fn2 = sum(ev_eig1[1:(c + 1)])
+    converge[iter] = fn2 - fn1
+    if (iter < 10) {
+      if (ev_eig1[length(ev_eig1)] > 1e-06) {
+        lambda = 1.5 * lambda
+        r = r/1.01
+      }
+    }
+    else {
+      if (converge[iter] > 1.01 * converge[iter - 1]) {
+        S = S_old
+        if (converge[iter - 1] > 0.2) {
+          warning("Maybe you should set a larger value of c.")
+        }
+        break
+      }
+    }
+    S_old = S
+    distX = D_Kernels[[1]] * alphaK[1]
+    for (i in 2:length(D_Kernels)) {
+      distX = distX + as.matrix(D_Kernels[[i]]) * alphaK[i]
+    }
+    res = apply(distX, MARGIN = 1, FUN = function(x) return(sort(x, 
+                                                                 index.return = TRUE)))
+    distX1 = array(0, c(nrow(distX), ncol(distX)))
+    idx = array(0, c(nrow(distX), ncol(distX)))
+    for (i in 1:nrow(distX)) {
+      distX1[i, ] = res[[i]]$x
+      idx[i, ] = res[[i]]$ix
+    }
+  }
+  LF = F_eig1
+  D = diag(apply(S, MARGIN = 2, FUN = sum))
+  L = D - S
+  eigen_L = eigen(L)
+  U = eigen_L$vectors
+  D = eigen_L$values
+  if (length(no.dim) == 1) {
+    U_index = seq(ncol(U), (ncol(U) - no.dim + 1))
+    F_last = CIMLR:::tsne(S, k = no.dim, initial_config = U[, U_index])
+  }
+  else {
+    F_last = list()
+    for (i in 1:length(no.dim)) {
+      U_index = seq(ncol(U), (ncol(U) - no.dim[i] + 1))
+      F_last[i] = list(CIMLR:::tsne(S, k = no.dim[i], initial_config = U[, 
+                                                                 U_index]))
+    }
+  }
+  execution.time = proc.time() - ptm
+  cat("Performing Kmeans.\n")
+  y = kmeans(F_last, c, nstart = 200)
+  ydata = CIMLR:::tsne(S)
+  results = list()
+  results[["y"]] = y
+  results[["S"]] = S
+  results[["F"]] = F_last
+  results[["ydata"]] = ydata
+  results[["alphaK"]] = alphaK
+  results[["execution.time"]] = execution.time
+  results[["converge"]] = converge
+  results[["LF"]] = LF
+  return(results)
+}
