96d8b3b8dbee00278493294e344e5e91bbce8d3b
Aris Sionakidis <as3582@cam.ac.uk>
2025-01-31 11:34:04 +0000

    PIntMF HPC prep

---
+++ Scripts/single_algorithm/PIntMF_HPC/PIntMF_ndim_4_maxiter_20_initflav_snf_modflav_glmnet.R
@@ -0,0 +1,32 @@
+
+suppressPackageStartupMessages(library(PintMF))
+
+# Load input data
+input = readRDS("PIntNMF_input.rds")
+
+RNGversion("4.2.2")
+set.seed(123)
+
+# Hyperparameter setup ###
+p = 4
+max.it = 20
+init_flavor = snf
+flavor_mod = glmnet
+
+# Run PIntNMF
+pintmf = SolveInt(
+  Y=input, 
+  p=p, 
+  max.it=max.it, 
+  verbose=FALSE, 
+  init_flavor=init_flavor, 
+  flavor_mod=flavor_mod
+)
+
+# Save the results
+saveRDS(pintmf, "PIntMF_ndim_4_maxiter_20_initflav_snf_modflav_glmnet_results.rds")
+
+# Also record sessionInfo
+writeLines(capture.output(sessionInfo()), 
+           "PIntMF_ndim_4_maxiter_20_initflav_snf_modflav_glmnet_sessionInfo.txt")
+
