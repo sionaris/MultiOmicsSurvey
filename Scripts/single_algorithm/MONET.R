bfdfc174bef3b6f6aca9a4006276f4028ace1396
Aris Sionakidis <as3582@cam.ac.uk>
2025-02-04 17:14:24 +0000

    Preliminary MONET commit

---
+++ Scripts/single_algorithm/MONET.R
@@ -0,0 +1,107 @@
+# Import data from gitignored "Resources/BRCA complete/" folder #####
+
+# This RDS object was produced using the Scripts/MOVICS/MOVICS_baseline.R script
+input = readRDS("Resources/TCGA/mm_input.rds")
+
+# Setup environment variables for markdown #####
+
+# Ensure reproducibility
+RNGversion("4.2.2")
+set.seed(123)
+
+# Load custom helper functions
+source("Scripts/automated_scripts/custom_functions.R")
+source("Scripts/automated_scripts/modified_MOVICS_functions.R")
+
+# Preamble
+home = getwd()
+algorithm = "MONET"
+alg_feature_pref = "cols" # Where does the algorithm expect the features to be
+citation = fetch_citation(algorithm = algorithm)
+data_source = "TCGA" # e.g. TCGA, TCGA-transNEO, transNEO-PARTNER
+data_types = "RNAseq-CNV-Methylation-miRNA-SNPs" # e.g. RNAseq, RNAseq-CNV-miRNA
+evaluation_source = "transNEO" # e.g. PARTNER, transNEO-PARTNER 
+title = paste0("Results from ", algorithm)
+subtitle = paste0("<b>Train</b>: ", data_source, " ", data_types, 
+                  " | <b>Evaluation</b>: ", evaluation_source)
+in_a_nutshell = fetch_in_a_nutshell(algorithm = algorithm)
+ground_truth_labels = openxlsx::read.xlsx("Results/MOVICS_baseline/MOVICS_TCGA_RNAseq-CNV-Methylation-miRNA-SNPs_eval_on_transNEO_clusterings.xlsx")
+ground_truth_k = 2 # optk from MOVICS
+optk_boolean = "TRUE" # either TRUE or FALSE. Answers whether the algorithm suggests an optimal k
+optk_text = ifelse(optk_boolean == TRUE,
+                   "<u>suggests</u> an estimate of the optimal number of multi-omic clusters $k$",
+                   "<u>does not suggest</u> an optimal number of multi-omic clusters $k$")
+
+# Detailed description of the algorithm
+description = paste(readLines(paste0("Resources/algorithm_descriptions/", algorithm,
+                                     "_description.Rmd")),
+                    collapse = "\n") # File path to .Rmd file within Resources/algorithm_descriptions
+
+# Create algorithm directory if it doesn't exist
+if (!dir.exists(paste0(home, "/Results/single_algorithm/", 
+                       algorithm))) {
+  dir.create(paste0(home, "/Results/single_algorithm/", 
+                    algorithm))
+}
+
+# Preprocessing flags and code #####
+library(stringr)
+library(dplyr)
+
+# All preprocessing for this input has already been performed using the 
+# Scripts/MOVICS/MOVICS_baseline.R script
+
+# Extract the names of the modalities that will be used
+modalities = unlist(strsplit(data_types, "-"))
+
+# Replace with TRUE wherever features are in rows
+features_in_rows = rep(TRUE, length(modalities))
+
+# Run algorithm #####
+# Check what kind of arrangement the algorithm requires as input 
+# (i.e. features in rows or columns)?
+if (alg_feature_pref == "rows") {
+  rogue_indices = which(features_in_rows == FALSE)
+  if (length(rogue_indices >= 1)) {
+    for (index in rogue_indices) {
+      cols = colnames(input[[index]])
+      rows = rownames(input[[index]])
+      input[[index]] = t(input[[index]])
+      rownames(input[[index]]) = cols # transpose names
+      colnames(input[[index]]) = rows # transpose names
+      rm(rows, cols)
+    }
+  }
+} else if (alg_feature_pref == "cols") {
+  rogue_indices = which(features_in_rows == TRUE)
+  if (length(rogue_indices >= 1)) {
+    for (index in rogue_indices) {
+      cols = colnames(input[[index]])
+      rows = rownames(input[[index]])
+      input[[index]] = t(input[[index]])
+      rownames(input[[index]]) = cols # transpose names
+      colnames(input[[index]]) = rows # transpose names
+      rm(rows, cols)
+    }
+  }
+}
+rm(rogue_indices, index); gc()
+
+# Import clinical data for the TCGA samples of interest
+clinical_data = openxlsx::read.xlsx("Resources/TCGA/clinical_data.xlsx")
+
+# Import results with reticulate #####
+library(reticulate)
+
+# Add the Python dir of this project to the Python path of reticulate
+py_run_string("import sys; sys.path.insert(0, 'Python/MONET')")
+
+py_run_string("import pickle")
+# py_install("networkx==3.2.1")
+# py_install("pandas==2.2.3")
+py_run_string("import networkx as nx")
+py_run_string("from monet import monet")
+py_run_string("monet_results = pickle.load(open('Python/MONET/monet_results/monet_output.pkl', 'rb')) ")
+
+# Create R variable
+MONET = py$monet_results
