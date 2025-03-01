84f4049cde073dc2a4387b020bb7f9fec4bdb65e
Aris Sionakidis <as3582@cam.ac.uk>
2025-03-01 16:18:25 +0000

    MOFA GPU scripts

---
+++ Scripts/single_algorithm/MOFA_GPU/bash_MOFA_ncp15.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH -p ampere
+#SBATCH -A SIMIDJIEVSKI-SL3-GPU
+#SBATCH --job-name=MOFA_ncp15
+#SBATCH --output=logs/MOFA_ncp15.out
+#SBATCH --error=logs/MOFA_ncp15.err
+#SBATCH --time=11:59:59
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=as3582@cam.ac.uk
+
+#! Number of nodes and tasks per node allocated by SLURM (do not change):
+numnodes=$SLURM_JOB_NUM_NODES
+numtasks=$SLURM_NTASKS
+mpi_tasks_per_node=$(echo "$SLURM_TASKS_PER_NODE" | sed -e  's/^\([0-9][0-9]*\).*$/\1/')
+
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp              # REQUIRED - loads the basic environment
+module load cuda/11.4
+
+# Insert additional module load commands after this line if needed:
+
+#! Full path to application executable:
+application="python MOFA_ncp15.py"
+
+#! Run options for the application:
+options=""
+
+#! Work directory (i.e. where the job will run):
+workdir="$SLURM_SUBMIT_DIR"  # The value of SLURM_SUBMIT_DIR sets workdir to the directory
+                             # in which sbatch is run.
+
+#! Are you using OpenMP (NB this is unrelated to OpenMPI)? If so increase this
+#! safe value to no more than 128:
+export OMP_NUM_THREADS=1
+
+#! Number of MPI tasks to be started by the application per node and in total (do not change):
+np=$[${numnodes}*${mpi_tasks_per_node}]
+
+#! Choose this for a pure shared-memory OpenMP parallel program on a single node:
+#! (OMP_NUM_THREADS threads will be created):
+CMD="$application $options"
+
+#! Choose this for a MPI code using OpenMPI:
+#CMD="mpirun -npernode $mpi_tasks_per_node -np $np $application $options"
+
+
+###############################################################
+### You should not have to change anything below this line ####
+###############################################################
+
+cd $workdir
+echo -e "Changed directory to `pwd`.
+"
+
+JOBID=$SLURM_JOB_ID
+
+echo -e "JobID: $JOBID
+======"
+echo "Time: `date`"
+echo "Running on master node: `hostname`"
+echo "Current directory: `pwd`"
+
+if [ "$SLURM_JOB_NODELIST" ]; then
+        #! Create a machine file:
+        export NODEFILE=`generate_pbs_nodefile`
+        cat $NODEFILE | uniq > machine.file.$JOBID
+        echo -e "\nNodes allocated:\n================"
+        echo `cat machine.file.$JOBID | sed -e 's/\..*$//g'`
+fi
+
+echo -e "\nnumtasks=$numtasks, numnodes=$numnodes, mpi_tasks_per_node=$mpi_tasks_per_node (OMP_NUM_THREADS=$OMP_NUM_THREADS)"
+
+echo -e "\nExecuting command:\n==================\n$CMD\n"
+
+source /home/as3582/miniconda3/etc/profile.d/conda.sh
+conda activate mofa_env_gpu
+mkdir -p logs
+eval $CMD
